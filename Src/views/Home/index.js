3d0169844197b31881b22c898dd391a06e8f2972
ETG01 <88471094+ETG01@users.noreply.github.com>
2022-12-06 15:58:29 +0000

    sss

--- Src/views/Home/index.js
+++ Src/views/Home/index.js
@@ -1,65 +1,24 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
   View,
-  StatusBar,
-  TouchableOpacity,
   Text,
 } from "react-native";
 import {
-  Image,
-  Center,
-  Popover,
-  Button,
-  Input,
   FormControl,
-  Box,
-  NativeBaseProvider,
-  Select,
-  CheckIcon
 } from "native-base";
-import cinemas from "../../resources/cinemas.json";
-// import DropDown from '../../components/DropDown';
-
-const getSelectionFromData = (CinemaData) => {
-  console.log(CinemaData);
-  return CinemaData.map((cinema) => {
-    return {
-      label: cinema.name,
-      value: cinema.id,
-    };
-  });
-};
-
-const DropDown = () => {
-  const [data, setdata] = React.useState(cinemas); 
-  const [service, setService] = React.useState("");
-  console.log(getSelectionFromData(data));
-  return <Center>
-      <Box maxW="300">
-        <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
-        bg: "teal.600",
-        endIcon: <CheckIcon size="5" />
-      }} mt={1} onValueChange={itemValue => setService(itemValue)}>
-          <Select.Item label="UX Research" value="ux" />
-          <Select.Item label="Web Development" value="web" />
-          <Select.Item label="Cross Platform Development" value="cross" />
-          <Select.Item label="UI Designing" value="ui" />
-          <Select.Item label="Backend Development" value="backend" />
-        </Select>
-      </Box>
-    </Center>;
-};
+import cinemasjason from "../../resources/cinemas.json";
+import DropDown from "../../components/DropDown";
 
 
 const Home = ({ navigation: {navigate} }) => {
-
+  const [CinemasData, setCinemasData] = React.useState(cinemasjason); 
+  const [ChosenCinemaId, setChosenCinemaId] = React.useState('all');
+  console.log('id: ',ChosenCinemaId)
   return (
     <View>
        <FormControl mt="3">
-       <DropDown/>
-        <Text>Home</Text>
+       <DropDown ChosenCinemaId={ChosenCinemaId} setChosenCinemaId={setChosenCinemaId} CinemasData={CinemasData} />
+        <Text>{ChosenCinemaId === 'all' ? CinemasData.map((item) => ' - ' + item.name) : CinemasData.find((item) => item.id === ChosenCinemaId).name}</Text>
         </FormControl>
     </View>
   );
