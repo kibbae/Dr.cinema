f8a87868c2e232ec1f8427bcc4d44923b9659c54
ETG01 <88471094+ETG01@users.noreply.github.com>
2022-12-08 17:21:22 +0000

    sss

--- Src/views/LoadPage/index.js
+++ Src/views/LoadPage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button,Image , Spinner, HStack, Center, NativeBaseProvider } from "native-base";
+import { View, Text, Button } from "native-base";
 import { useSelector, useDispatch } from "react-redux";
 import store from "../../DataSystem/Redux/store";
 import { useNavigation } from "@react-navigation/native";
@@ -17,8 +17,6 @@ const LoadPage = () => {
         if (AllDataLoaded.loaded === true) 
         {
             //wait for half a second
-            // wait for 1 milisecond
-
             setTimeout(() => {
                 navigate("Home");
             }, 5000);  
@@ -29,14 +27,11 @@ const LoadPage = () => {
     //checkLoading();
 
     return (
-        <View backgroundColor={'#263238'} height='full' width='full'>
-            <Center marginTop={'2/4'} >
-                <Image size={'2xl'} source={require('../../resources/popi2.gif')} alt="Loading" />
-            </Center>
-            <Center >
-            <Image onPress={checkLoading()} size={'sm'} width='200' height='100' source={require('../../resources/loding.gif')} alt="Loading" />
-            </Center>
-        </View>
+        <SafeAreaView>
+        <Text>LoadPage</Text>
+        <Text>{AllDataLoaded.loaded}</Text>
+        <Button title="Test" onPress={checkLoading()}/>
+        </SafeAreaView>
     )
 
         
--- Src/views/Upcoming/index.js
+++ Src/views/Upcoming/index.js
@@ -4,6 +4,7 @@ import {
   View,
   Text,
   TouchableOpacity,
+  Image
 } from "react-native";
 import {
   FormControl,
