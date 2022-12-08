77c5a6683330c7fc648bc048213001929118503a
ETG01 <88471094+ETG01@users.noreply.github.com>
2022-12-08 17:27:18 +0000

    ssss

--- Src/views/Home/index.js
+++ Src/views/Home/index.js
@@ -23,17 +23,12 @@ import { fetchMovies } from "../../DataSystem/Redux/store"
 
 const Home = () => {
   const state = useSelector(state => state)
-<<<<<<< HEAD
-  const [data, setData] = useState(state);
-  const [CinemasData, setCinemasData] = React.useState(data["Cinemas"].Data); 
-=======
   const [data, setData] = useState(state)
   const notFixallMovies = data['Movies']
   const allMovies = notFixallMovies.Data
   console.log('shittt ', allMovies)
 
   const [CinemasData, setCinemasData] = React.useState(cinemasjason); 
->>>>>>> parent of cc2dde70 (Merge branch 'AboutCinema')
   const [ChosenCinemaId, setChosenCinemaId] = React.useState('all');
 
   // for Filter by cinemas
