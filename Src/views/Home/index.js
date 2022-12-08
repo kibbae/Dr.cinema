db8042571e122466de69724b6b01fba614d8f795
ETG01 <88471094+ETG01@users.noreply.github.com>
2022-12-08 17:29:21 +0000

    Revert "Merge branch 'main' into dem"
    This reverts commit e6377cb95b8c9ea1d7cd8c7e582dce9d62e32e5e, reversing
    changes made to e413e5a5686fc226bae8cd60e5ea16420623309a.

--- Src/views/Home/index.js
+++ Src/views/Home/index.js
@@ -24,11 +24,7 @@ import { fetchMovies } from "../../DataSystem/Redux/store"
 const Home = () => {
   const state = useSelector(state => state)
   const [data, setData] = useState(state)
-  const notFixallMovies = data['Movies']
-  const allMovies = notFixallMovies.Data
-  console.log('shittt ', allMovies)
-
-  const [CinemasData, setCinemasData] = React.useState(cinemasjason); 
+  const [CinemasData, setCinemasData] = React.useState(data["Cinemas"].Data); 
   const [ChosenCinemaId, setChosenCinemaId] = React.useState('all');
 
   // for Filter by cinemas
